655144f68cdb59314fb9db76f04198ce6790380d
Jahidul Zaid <jahidulzaid@gmail.com>
2022-07-24 23:00:14 +0600

    edited lcm

--- 13. lcm.c
+++ 13. lcm.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main(){
-int i,
+int n1, n2, lcm, max;
+printf("Enter two num to check LCM: ");
+scanf("%d %d", &n1, &n2);
+max = (n1>n2) ? n1: n2;
+while(1){
+    if(max %n1 == 0 && max % n2 == 0)
+    {
+
+        printf("The LCM is %d", lcm);
+        break;
+    }
+    max++;
+    lcm = max;
+}
 }
