079f49ed4936673ffced7e29aa18466ec5e2cc4e
Jahidul Zaid <jahidulzaid@gmail.com>
2022-07-25 21:52:26 +0600

    Edited 16

--- 16. reverse a number.c
+++ 16. reverse a number.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
 int main(){
-int i=0, num;
+int num, remainder, reverse = 0, original;
 printf("Enter a number: ");
 scanf("%d", &num);
+original = num;
 
 while(num != 0){
+    remainder = num % 10;
+    reverse = reverse *10 + remainder;
     num /=10;
-    i++;
 }
-printf("%d", i);
+printf("%d\n", reverse);
 
+if(reverse == original)
+    printf("The input num is palindrome.\n");
+else
+    printf("The input num is not palindrome.\n");
 }
