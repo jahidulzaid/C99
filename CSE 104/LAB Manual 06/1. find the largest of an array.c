023b02b99a56c9724a3a03d56538e39f44a193d4
Jahidul Zaid <jahidulzaid@gmail.com>
2022-08-11 02:15:15 +0600

    Added CSE104

--- CSE 104/LAB Manual 06/1. find the largest of an array.c
+++ CSE 104/LAB Manual 06/1. find the largest of an array.c
@@ -6,10 +6,16 @@ int main(){
 
 void largestInArr(){
     // read arr elemnts
-    int Arr[5];
-    for (int i=0; i <= 4; i++){
-       scanf("%d", &Arr[i]);
-       printf("%d\n", Arr[i]);
+    int n;
+    printf("Enter the size of the Arry: ");
+    scanf("%d", &n);
+    int Arr[n];
+    for (int i=0; i <= n; i++){
+        {
+            printf("Enter the element of %d: ", i);
+            scanf("%d", &Arr[i]);
+        }
+       
     }
     // finding largest
     int large = Arr[0];
