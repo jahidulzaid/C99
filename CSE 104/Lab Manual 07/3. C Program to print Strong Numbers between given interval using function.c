c83222738c36cf36bed26e273538cdbbb272d68a
Jahidul Zaid <jahidulzaid@gmail.com>
2022-08-28 12:36:33 +0600

    Added soln in man 7

--- CSE 104/Lab Manual 07/3. C Program to print Strong Numbers between given interval using function.c
+++ CSE 104/Lab Manual 07/3. C Program to print Strong Numbers between given interval using function.c
@@ -4,38 +4,36 @@ long long fact(int temp);
 
 int main(){
     int n1, n2;
-    printf("Enter the starting number: ");
+    printf("Enter starting point: ");
     scanf("%d", &n1);
-    printf("Enter the ending number: ");
+    printf("Enter ending point: ");
     scanf("%d", &n2);
     strong_number(n1, n2);
     return 0;
 }
+
 void strong_number(int n1, int n2){
-    int i, j, temp, sum;
-    //iterates from n1 to n2
-    while(n1 !=n2){
-        sum = 0;
+    int i, j, sum, temp;
+    
+    while (n1 != n2)
+    {
         temp = n1;
-        //calculates sum of factorial of digits of n1
-        while(temp != 0){
+        sum = 0;
+        while (temp != 0)
+        {
             sum += fact(temp % 10);
             temp /= 10;
         }
-        if (n1 == sum){
+        if ( n1 == sum)
+        {
             printf("%d, ", n1);
         }
-
         n1++;
-       
-
     }
 }
-//recursive function to find factorial of a number
-long long fact(int temp)
-{
-    if(temp == 0)
+long long fact (int temp){
+    if (temp == 0)
         return 1;
     else
-        return (temp * fact(temp-1));
+        return (temp * fact(temp -1));
 }
