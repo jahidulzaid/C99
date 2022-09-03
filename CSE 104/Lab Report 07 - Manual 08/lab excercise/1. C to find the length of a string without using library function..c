52607430a11c6494295f91d8213e856bb9abca2f
Jahidul Zaid <jahidulzaid@gmail.com>
2022-09-04 01:36:52 +0600

    added cse104 man8

--- CSE 104/Lab Report 07 - Manual 08/lab excercise/1. C to find the length of a string without using library function..c
+++ CSE 104/Lab Report 07 - Manual 08/lab excercise/1. C to find the length of a string without using library function..c
@@ -1,14 +1,16 @@
 #include<stdio.h>
-void stringLength(char str);
+void stringLength(char str[]);
 int main(){
     char str[100];
     printf("Enter string: ");
     fgets(str, sizeof(str), stdin);
     stringLength(str);
+    
     return 0;
 }
-void stringLength(char str){
+void stringLength(char str[]){
     int i;
     for (i=0; str[i]!='\0';i++){
     }
+    printf("Length of string is: %d", i);
 }
--- CSE 104/Lab Report 07 - Manual 08/lab excercise/3. C to extract a substring from a given string.c
+++ CSE 104/Lab Report 07 - Manual 08/lab excercise/3. C to extract a substring from a given string.c
@@ -1,4 +1,22 @@
 #include<stdio.h>
+int substring(char str[], char sstr[], int start, int end);
 int main(){
-    
+    char str[100], sstr[100];
+    int start, end;
+    printf("Enter string: ");
+    fgets(str, sizeof(str), stdin);
+    printf("Enter start index: ");
+    scanf("%d", &start);
+    printf("Enter end index: ");
+    scanf("%d", &end);
+    substring(str, sstr, start, end);
+}   
+
+int substring(char str[], char sstr[], int start, int end){
+    int i, j;
+    for (i=start, j=0; i<=end; i++, j++){
+        sstr[j]=str[i];
+    }
+    sstr[j]='\0';
+    printf("Substring is: %s", sstr);
 }
